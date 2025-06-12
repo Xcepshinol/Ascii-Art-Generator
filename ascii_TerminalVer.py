9444626ba60f7d2872e0c0a2d7fe19ae45876339
Davis <yewk220@gmail.com>
2025-06-12 16:24:35 -0400

    Made the terminal version work with the webcam

--- ascii_TerminalVer.py
+++ ascii_TerminalVer.py
@@ -1,27 +1,49 @@
-from PIL import Image, ImageDraw, ImageFont
+import cv2, os, platform
 
-def draw(input_im,text_file):
-    lett = "@#$%&*+-. " #character corresponding to brightness
+def clear_term():
+    if platform.system() == 'Windows':
+        os.system("cls")
+    else:
+        os.system("clear")
 
-    im_b = Image.open(input_im).convert("L") #convert to grayscale
-    w1,l1 = im_b.size #pixel width and length of image
-    aspect = l1/w1
+def webcam():
+    lett = "@#$%&*+-.  "
 
-    w = 100 #resized width constant
-    l = int(aspect*100*0.55) #corresponding length
+    cap = cv2.VideoCapture(0)
+    
+    if not cap.isOpened():
+        raise IOError("Cannot open webcam")
 
-    im = im_b.resize((w,l)) #resized image
+    
+    while True:
+        s=''
 
-    for i in range(l):
-        s =''
-        for j in range(w):
-            x = (255-(im.getpixel((j,i))))//25 #find the character corresponding brightness
-            s+=lett[x-1]
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        w = 100
+
+        l1, w1 = frame.shape[:2]
+        l = int((l1 / w1) * w * 0.5)
+
+        frame = cv2.resize(frame, (w,l), interpolation=cv2.INTER_AREA)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        clear_term()
+        for i in range(l):
+            s += '\n'
+            for j in range(w):
+                x = (255-(frame[i, j]))//25 #find the character corresponding brightness
+                s+=lett[x]        
         print(s)
-    return
 
-#local variables
-input_im = "test.jpg"
-text_file = "test_output.txt"
+        cv2.imshow('test', frame)
 
-draw(input_im, text_file)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+webcam()
