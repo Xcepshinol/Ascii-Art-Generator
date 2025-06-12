59f969e952d0eb342792e991a20b020935e83c1b
Davis <yewk220@gmail.com>
2025-06-12 13:57:54 -0400

    added the option to print to a txt file or just display in the terminal

--- ascii.py
+++ ascii.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def draw(input_im,text_file):
+def draw(input_im,text_file=False):
     lett = "@#$%&*+-. " #character corresponding to brightness
 
     oa = []
@@ -9,30 +9,36 @@ def draw(input_im,text_file):
     w1,l1 = im_b.size #pixel width and length of image
     aspect = l1/w1
 
-    w = 250 #resized width constant
-    l = int(aspect*250*0.55) #corresponding length
+    w = 100 #resized width constant
+    l = int(aspect*w*0.55) #corresponding length
 
     im = im_b.resize((w,l)) #resized image
 
-    for i in range(l):
-        oa.append([]) #initialize character matrix
-        for j in range(w):
-            x = (255-(im.getpixel((j,i))))//25 #find the character corresponding brightness
-            oa[i].append(lett[x-1]) #add character to character matrix
+    try:
+        for i in range(l):
+            oa.append([]) #initialize character matrix
+            for j in range(w):
+                x = (255-(im.getpixel((j,i))))//25 #find the character corresponding brightness
+                oa[i].append(lett[x-1]) #add character to character matrix
 
-    with open(text_file,'w') as imat: #write to txt file
-        for k in range(len(oa)):
-            for p in range(len(oa[k])):
-                imat.write(oa[k][p])
-            imat.write("\n") #space for each line in character matrix
-
+        with open(text_file,'w') as imat: #write to txt file
+            for k in range(len(oa)):
+                for p in range(len(oa[k])):
+                    imat.write(oa[k][p])
+                imat.write("\n") #space for each line in character matrix
+    except:
+        for i in range(l):
+            s =''
+            for j in range(w):
+                x = (255-(im.getpixel((j,i))))//25 #find the character corresponding brightness
+                s+=lett[x-1]
+            print(s)
     return
 
 #local variables
 input_im = "test.jpg"
-text_file = "test_output.txt"
 
-draw(input_im, text_file)
+draw(input_im)
     
 
 
